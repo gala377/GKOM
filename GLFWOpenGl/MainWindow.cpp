de6aab9f13fa75adff24c13f0750ade77a6f2e19
gala377 <r.galczak@gmail.com>
2017-05-21 08:29:08 +0200

    Brief

--- GLFWOpenGl/MainWindow.cpp
+++ GLFWOpenGl/MainWindow.cpp
@@ -2,9 +2,12 @@
 
 
 
-MainWindow::MainWindow()
+MainWindow::MainWindow(int width = 800, int height = 600, std::string title = "")
 {
-}
+	std::cout << "Creating new window\n";
+	window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+	
+} 
 
 
 MainWindow::~MainWindow()
--- GLFWOpenGl/MainWindow.h
+++ GLFWOpenGl/MainWindow.h
@@ -1,8 +1,16 @@
 #pragma once
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 class MainWindow
 {
 public:
-	MainWindow();
+	MainWindow(int width, int height, std::string title);
 	~MainWindow();
+private:
+	GLFWwindow* window;
+
+
 };
 
